51b06d376fdff75273442967b5634796391625b6
Mst. Shikha Khatun <128407529+mstshikha@users.noreply.github.com>
2024-04-07 10:42:29 +0600

    Add files via upload

---
+++ src/Home/Home.jsx
@@ -0,0 +1,18 @@
+import About from "./About";
+import Banner from "./Banner";
+import Slider from "./Slider";
+import Team from "./Team";
+
+
+const Home = () => {
+    return (
+        <div>
+           <Banner></Banner>
+           <Slider></Slider>
+           <About></About>
+           <Team></Team>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Main/Main';
+import Home from './Home/Home';
+import Category from './Category/Category';
+import Guide from './Guide/Guide';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },{
+        path:'/category',
+        element:<Category></Category>
+      },
+      {
+        path:'/guideline',
+        element:<Guide></Guide>
+      }
+    ]
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
